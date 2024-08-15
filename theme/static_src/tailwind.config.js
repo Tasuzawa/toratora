31752892f65c382ab262285339887a97c806ed76
taufiksuherlan1003 <taufiksuherlan1003@gmail.com>
2024-08-15 03:11:26 +0000

    update

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -40,6 +40,7 @@ module.exports = {
          * and make sure the pattern below matches your project structure.
          */
         // '../../**/*.py'
+        'node_modules/preline/dist/*.js',
     ],
     theme: {
         extend: {},
@@ -53,5 +54,6 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
+        require('preline/plugin'),
     ],
 }
