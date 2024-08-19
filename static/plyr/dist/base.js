4f5818a772afe069d072c55cc9f6027e60cd3de9
taufiksuherlan1003 <taufiksuherlan1003@gmail.com>
2024-08-19 07:57:32 +0000

    Changes to be committed: 	modified:   core/templates/animeDetail.html 	modified:   core/templates/animePlayEpisode.html 	modified:   core/templates/cotton/example/anime/playvideo/plyr.html 	modified:   core/templates/cotton/layout/playvideo/base.html 	modified:   core/templates/cotton/libs/public/plyr/js.html 	modified:   core/templates/main.html 	new file:   static/plyr/dist/base.js

--- static/plyr/dist/base.js
+++ static/plyr/dist/base.js
@@ -1,11 +1,6 @@
-{% load static %}
-
-
-<script src="{% static 'plyr/dist/plyr.js' %}"></script>
-<script>
+// Change the second argument to your options:
+// https://github.com/sampotts/plyr/#options
 const player = new Plyr('video', {captions: {active: true}});
 
 // Expose player so it can be used from the console
 window.player = player;
-
-</script>
