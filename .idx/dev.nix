4fc27c62b830907da608eecaa88a1042bd578c1a
taufiksuherlan1003 <taufiksuherlan1003@gmail.com>
2024-08-20 04:29:44 +0000

    update

---
+++ .idx/dev.nix
@@ -0,0 +1,65 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    # pkgs.go
+     pkgs.python311
+    # pkgs.python311Packages.pip
+     pkgs.nodejs_20
+    # pkgs.nodePackages.nodemon
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "batisteo.vscode-django"
+      "BeardedBear.beardedtheme"
+      "bradlc.vscode-tailwindcss"
+      "donjayamanne.python-environment-manager"
+      "donjayamanne.python-extension-pack"
+      "KevinRose.vsc-python-indent"
+      "ms-python.debugpy"
+      "ms-python.python"
+      "njpwerner.autodocstring"
+      "wholroyd.jinja"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = false;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
